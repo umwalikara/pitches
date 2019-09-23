510c95769f280fc9f48953fd7296c965caf3f3a0
umwalikara <umwaliclara@gmail.com>
2019-09-23 10:17:31 +0200

    addind root function for product

--- app/main/views.py
+++ app/main/views.py
@@ -59,14 +59,14 @@ def promotion():
     return render_template('promotion.html', title = title, pitches= pitches )
 
 
-# @main.route('/product/pitches/')
-# def product():
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     title = 'Product Pitches'
-#     pitches= Pitch.get_all_pitches()
-#     return render_template('product.html', title = title, pitches= pitches )
+@main.route('/product/pitches/')
+def product():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Product Pitches'
+    pitches= Pitch.get_all_pitches()
+    return render_template('product.html', title = title, pitches= pitches )
  
 #  end of category root functions
 
