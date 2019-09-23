892b8b0f58c0f72ad85fb9380d21f24a48ab2da0
umwalikara <umwaliclara@gmail.com>
2019-09-23 10:01:54 +0200

    adding configuration

--- config.py
+++ config.py
@@ -1,41 +1,41 @@
-# import os
+import os
 
-# class Config:
+class Config:
 
-#     SECRET_KEY = 'a random string'
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch'
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-#     SUBJECT_PREFIX = 'picthing'
-#     # SENDER_EMAIL = 'akimanizanye.claudine@gmail.com'
+    SECRET_KEY = 'a random string'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'picthing'
+    # SENDER_EMAIL = 'akimanizanye.claudine@gmail.com'
 
-#     # simple mde  configurations
-#     SIMPLEMDE_JS_IIFE = True
-#     SIMPLEMDE_USE_CDN = True
-
-
-#     @staticmethod
-#     def init_app(app):
-#         pass
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
-# class ProdConfig(Config):
-#     pass
+    @staticmethod
+    def init_app(app):
+        pass
 
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch_test'
 
-# class DevConfig(Config):
-#     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch'
-#     DEBUG = True
+class ProdConfig(Config):
+    pass
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch_test'
+
+class DevConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:kasa@10@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
