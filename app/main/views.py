1d860aa6658f15c7d9b14f8be8940716003e80d4
umwalikara <umwaliclara@gmail.com>
2019-09-23 10:19:28 +0200

    addind root function for category

--- app/main/views.py
+++ app/main/views.py
@@ -114,19 +114,19 @@ def new_pitch():
 
     return render_template('new_pitch.html', new_pitch_form= form, category= category)
 
-# @main.route('/category/<int:id>')
-# def category(id):
-#     '''
-#     function that returns pitches based on the entered category id
-#     '''
-#     category = PitchCategory.query.get(id)
-#     Comments.query.filter_by(pitch_id=id)
+@main.route('/category/<int:id>')
+def category(id):
+    '''
+    function that returns pitches based on the entered category id
+    '''
+    category = PitchCategory.query.get(id)
+    Comments.query.filter_by(pitch_id=id)
 
-#     if category is None:
-#         abort(404)
+    if category is None:
+        abort(404)
 
-#     pitches_in_category = Pitches.get_pitch(id)
-#     return render_template('category.html' ,category= category, pitches= pitches_in_category)
+    pitches_in_category = Pitches.get_pitch(id)
+    return render_template('category.html' ,category= category, pitches= pitches_in_category)
 
 # @main.route('/pitch/comments/new/<int:id>',methods = ['GET','POST'])
 # @login_required
