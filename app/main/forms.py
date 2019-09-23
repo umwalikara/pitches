bf080689771849fb2d062c25649c4f7fbdeeb167
umwalikara <umwaliclara@gmail.com>
2019-09-23 10:23:58 +0200

    addind form for creating pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,25 +9,25 @@ from ..models import User
 #     review = TextAreaField('Movie review', validators=[Required()])
 #     submit = SubmitField('Submit')
 
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('Tell us about you.',validators = [Required()])
-#     submit = SubmitField('Submit')
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
 
-# class CommentsForm(FlaskForm):
-#     comment = TextAreaField('Comment', validators=[Required()])
-#     vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
-#     submit = SubmitField('SUBMIT')  
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')  
 
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('we need your comment.',validators = [Required()])
-#     submit = SubmitField('Submit') 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('we need your comment.',validators = [Required()])
+    submit = SubmitField('Submit') 
 
-# class PitchForm(FlaskForm):
-#     category_id = SelectField('Select Category', choices=[('1', 'Story'), ('2', 'picking'), ('3', 'publicity'),('4','selling products')])
-#     content = TextAreaField('YOUR PITCH')
-#     submit = SubmitField('Create Pitch')
+class PitchForm(FlaskForm):
+    category_id = SelectField('Select Category', choices=[('1', 'Story'), ('2', 'picking'), ('3', 'publicity'),('4','selling products')])
+    content = TextAreaField('YOUR PITCH')
+    submit = SubmitField('Create Pitch')
 
-# class UpvoteForm(FlaskForm):
+class UpvoteForm(FlaskForm):
     
     
-#     submit = SubmitField('Upvote')
+    submit = SubmitField('Upvote')
