5dfba3f7e23f19cca42dc56031c89753a4188ff5
umwalikara <umwaliclara@gmail.com>
2019-09-23 09:59:09 +0200

    adding model class for comment table

--- app/models.py
+++ app/models.py
@@ -72,30 +72,30 @@ class Pitch(db.Model):
         return Pitch.query.filter_by(category_id= cat_id)
 
 
-# class Comment(db.Model):
+class Comment(db.Model):
 
-#     __tablename__ = 'comments'
+    __tablename__ = 'comments'
 
-#     id = db.Column(db.Integer,primary_key = True)
-#     comment= db.Column(db.String)
-#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
-#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-#     username =  db.Column(db.String)
-#     votes= db.Column(db.Integer)
+    id = db.Column(db.Integer,primary_key = True)
+    comment= db.Column(db.String)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    username =  db.Column(db.String)
+    votes= db.Column(db.Integer)
 
-#     def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
         
-#     @classmethod
-#     def clear_comments(cls):
-#         Comment.all_comments.clear()
+    @classmethod
+    def clear_comments(cls):
+        Comment.all_comments.clear()
 
-#     @classmethod
-#     def get_comments(cls,id):
-#         comments = Comment.query.filter_by(pitch_id=id).all()
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
 
-#         return comments
+        return comments
 
 # class Category(db.Model):
 #     '''
