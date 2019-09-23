55ff262cd43cf4e9e9eba7648efbb2b99db1e91d
umwalikara <umwaliclara@gmail.com>
2019-09-23 10:04:44 +0200

    addind manage file for run

--- manage.py
+++ manage.py
@@ -1,33 +1,33 @@
-# from flask_script import Manager,Server
-# from app import create_app,db
-# # from app.models import User,Role
-# from  flask_migrate import Migrate, MigrateCommand
-# from app.models import User
-# from flask_login import LoginManager
-# # from app.models import User,Pitch,Comment,Vote,PitchCategory
+from flask_script import Manager,Server
+from app import create_app,db
+# from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User
+from flask_login import LoginManager
+# from app.models import User,Pitch,Comment,Vote,PitchCategory
 
 
-# # Creating app instance
-# app = create_app('development')
+# Creating app instance
+app = create_app('development')
 
-# manager = Manager(app)
-# manager.add_command('server',Server)
+manager = Manager(app)
+manager.add_command('server',Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
-# app = create_app('test')
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+app = create_app('test')
 
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app,db = db,User = User,Role = Role )
-#     # return dict(app = app,db = db,User = User ,Pitch = Pitch,Comment = Comment,Vote = Vote,PitchCategory = PitchC
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Role = Role )
+    # return dict(app = app,db = db,User = User ,Pitch = Pitch,Comment = Comment,Vote = Vote,PitchCategory = PitchC
 
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
