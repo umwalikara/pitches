f5eb909f3b5bc0359f4de74fc129183127b14fff
umwalikara <umwaliclara@gmail.com>
2019-09-23 09:57:12 +0200

    adding user model class

--- app/models.py
+++ app/models.py
@@ -5,43 +5,43 @@ from . import login_manager
 from datetime import datetime
 
 
-# class User(UserMixin, db.Model):
-#     __tablename__ = 'users'
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(255), index=True)
-#     email = db.Column(db.String(255), unique=True, index=True)
-#     # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     password_hash = db.Column(db.String(255))
-#     pass_secure = db.Column(db.String(255))
-#     pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
-#     comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
-#     categories=db.relationship('Category',backref = 'user',lazy="dynamic")
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
+    categories=db.relationship('Category',backref = 'user',lazy="dynamic")
 
-#     def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
 
-#     @classmethod
-#     def get_comments(cls,id):
-#         reviews = Comment.query.filter_by(pitch_id=id).all()
-#         return comments
+    @classmethod
+    def get_comments(cls,id):
+        reviews = Comment.query.filter_by(pitch_id=id).all()
+        return comments
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-#     @password.setter
-#     def password(self, password):
-#         self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
-#     def verify_password(self, password):
-#         return check_password_hash(self.pass_secure, password)
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
-#     def __repr__(self):
-#         return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 # class Pitch(db.Model):
